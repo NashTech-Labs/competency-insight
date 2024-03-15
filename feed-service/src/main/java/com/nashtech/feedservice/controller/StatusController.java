7987c3ac8c9b4012e8050900adfbce3f3efdb133
SushantGupta <sushant.gupta@knoldus.com>
2024-03-15 17:00:29 +0530

    testing fix

--- feed-service/src/main/java/com/nashtech/feedservice/controller/StatusController.java
+++ feed-service/src/main/java/com/nashtech/feedservice/controller/StatusController.java
@@ -5,10 +5,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/feed")
 public class StatusController {
 
+
     @GetMapping
+    public String appRunningRootLevel(){
+        return "Contribution Service is Running...at root path";
+    }
+
+    @GetMapping
+    @RequestMapping("/feed")
     public String appRunning(){
         return "Feed Service is Running...";
     }
